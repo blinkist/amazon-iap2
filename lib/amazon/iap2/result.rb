92408eaf62f60b46f0542e4f07776476efd9a85d
pirvudoru <pirvu.doru@ymail.com>
2019-01-05 15:10:46 +0200

    Exctract helper method to parse dates

--- lib/amazon/iap2/result.rb
+++ lib/amazon/iap2/result.rb
@@ -36,15 +36,9 @@ class Amazon::Iap2::Result
 
       raise Amazon::Iap2::Exceptions::EmptyResponse unless parsed
 
-      if parsed.has_key? 'purchaseDate'
-        parsed['purchaseTime'] = parsed['purchaseDate'].nil? ? nil : Time.at(parsed['purchaseDate'] / 1000)
-      end
-      if parsed.has_key? 'cancelDate'
-        parsed['cancelTime'] = parsed['cancelDate'].nil? ? nil : Time.at(parsed['cancelDate'] / 1000)
-      end
-      if parsed.has_key? 'renewalDate'
-        parsed['renewalTime'] = parsed['renewalDate'].nil? ? nil : Time.at(parsed['renewalDate'] / 1000)
-      end
+      parse_time!(parsed, 'purchase')
+      parse_time!(parsed, 'cancel')
+      parse_time!(parsed, 'renewal')
 
       parsed.each do |key, value|
         underscore = key.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').gsub(/([a-z\d])([A-Z])/, '\1_\2').tr('-', '_').downcase
@@ -57,4 +51,15 @@ class Amazon::Iap2::Result
     else raise Amazon::Iap2::Exceptions::General
     end
   end
+
+  private
+
+    def parse_time!(json, date_key_prefix)
+      date_key = "#{date_key_prefix}Date"
+      time_key = "#{date_key_prefix}Time"
+
+      if json.has_key?(date_key)
+        json[time_key] = json[date_key].nil? ? nil : Time.at(json[date_key] / 1000)
+      end
+    end
 end
